2d47532aeffb95d546ba7d6934c04cd1d495e818
yesideviana <33503308+yesideviana@users.noreply.github.com>
2025-05-01 12:05:06 +0700

    Update lat.py

--- lat.py
+++ lat.py
@@ -2,4 +2,7 @@ import streamlit as st
 st.title('Obesity Prediction based on Eating Habits and Physical Condition')
 tinggi = st.number_input("Masukkan Tinggi Badan", 0)
 berat = st.number_input("Masukkan Berat Badan", 0)
-hitung = st.button('Hitung IMT')
+kalori = st.number_input("Masukkan Kalori harian", 0)
+frekuensi = st.number_input("Masukkan Frekuensi Makan Berat", 0)
+hitung = st.button('Indeks Massa Tubuh= berat/(tinggi)^2')
+hitung = st.button('Prediksi Menderita Obesitas')
