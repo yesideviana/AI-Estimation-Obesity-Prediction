a03bf43ff3a6627000287eaeb79f4940c4cce085
yesideviana <33503308+yesideviana@users.noreply.github.com>
2025-05-01 14:29:28 +0700

    Update lat.py

--- lat.py
+++ lat.py
@@ -2,9 +2,36 @@ import streamlit as st
 st.title('Obesity Prediction based on Eating Habits and Physical Condition')
 tinggi = st.number_input("Masukkan Tinggi Badan", 0)
 berat = st.number_input("Masukkan Berat Badan", 0)
-kalori = st.number_input("Apakah mengkonsumsi makanan tinggi Kalori", 0)
-sayur = st.bar_chart("Apakah ada sayuran",)
-frekuensi = st.set_option("Masukkan Frekuensi Makan Berat", 0)
+col1, col2 = st.columns(2)
+
+with col1 :
+    Berat = st.text_input ('input nilai berat badan')
+
+with col2 :
+    tinggi = st.text_input ('input nilai tinggi badan')
+
+with col1 :
+    kalori = st.text_input ('input nilai kalori harian')
+
+with col2 :
+    fisik = st.text_input ('input nilai Aktifitas Fisik')
+
+with col1 :
+    Insulin = st.text_input ('input nilai Insulin')
+
+with col2 :
+    BMI = st.text_input ('input nilai BMI')
+
+with col1 :
+    DiabetesPedigreeFunction = st.text_input ('input nilai Diabetes Pedigree Function')
+
+with col2 :
+    Age = st.text_input ('input nilai Age')
+
+# code untuk prediksi
+diab_diagnosis = ''
+
+# membuat tombol untuk prediksi
 if st.button('Test Prediksi Diabetes'):
     diab_prediction = diabetes_model.predict([[Berat, tinggi, kalori, fisik, Insulin, BMI, DiabetesPedigreeFunction, Age]])
 
