8448ee1c09a41a323b863e7a9ac946de6a39479c
yesideviana <33503308+yesideviana@users.noreply.github.com>
2025-05-01 15:01:37 +0700

    Update lat.py

--- lat.py
+++ lat.py
@@ -3,25 +3,25 @@ st.title('Obesity Prediction based on Eating Habits and Physical Condition')
 
 col1, col2 = st.columns(2)
 with col1 :
-Berat = st.text_input("Masukkan Berat Badan")
+Berat = st.number("Masukkan Berat Badan", )
 with col2 :
-Tinggi = st.text_input("Masukkan Berat Badan")
+Tinggi = st.number("Masukkan Berat Badan", )
 with col1 :
-kalori = st.text_input ("input nilai kalori harian")
+kalori = st.number ("input nilai kalori harian")
 with col2 :
-fisik = st.text_input ("input nilai Aktifitas Fisik")
+fisik = st.number ("input nilai Aktifitas Fisik")
 
    with col1 :
-    Insulin = st.text_input ('input nilai Insulin')
+    Insulin = st.number ('input nilai Insulin')
 
 with col2 :
-    BMI = st.text_input ('Apakah ada Aktifitas Fisik')
+    BMI = st.number ('Apakah ada Aktifitas Fisik')
 
 with col1 :
-    ObesPedigreeFunction = st.text_input ('Gaya Hidup Anda')
+    ObesPedigreeFunction = st.number ('Gaya Hidup Anda')
 
 with col2 :
-    Age = st.text_input ('input nilai Age')
+    Age = st.number ('input nilai Age')
 
 # code untuk prediksi Obes
 
