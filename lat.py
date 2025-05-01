650a9c1a1bda1bb8174567a5a46a848ecb0f240e
yesideviana <33503308+yesideviana@users.noreply.github.com>
2025-05-01 14:27:11 +0700

    Update lat.py

--- lat.py
+++ lat.py
@@ -3,7 +3,7 @@ st.title('Obesity Prediction based on Eating Habits and Physical Condition')
 tinggi = st.number_input("Masukkan Tinggi Badan", 0)
 berat = st.number_input("Masukkan Berat Badan", 0)
 kalori = st.number_input("Apakah mengkonsumsi makanan tinggi Kalori", 0)
-sayur = st_button("Apakah ada sayuran",)
+sayur = st.bar_chart("Apakah ada sayuran",)
 frekuensi = st.set_option("Masukkan Frekuensi Makan Berat", 0)
 if st.button('Test Prediksi Diabetes'):
     diab_prediction = diabetes_model.predict([[Berat, tinggi, kalori, fisik, Insulin, BMI, DiabetesPedigreeFunction, Age]])
