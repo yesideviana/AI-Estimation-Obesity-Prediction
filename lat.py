854ac2c7f19eb7233301fa7da6f52f41c2f86a0d
yesideviana <33503308+yesideviana@users.noreply.github.com>
2025-05-01 14:44:26 +0700

    Update lat.py

--- lat.py
+++ lat.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('Obesity Prediction based on Eating Habits and Physical Condition')
-st.background_color('blue')
+background : rgb
 col1, col2 = st.columns(2)
 tinggi = st.number_input("Masukkan Tinggi Badan", 0)
 berat = st.number_input("Masukkan Berat Badan", 0)
